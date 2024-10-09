a7191d482345952a3ad64ca6fee3a6ac477443bf
kljohnsonn <moxxydev@gmail.com>
2024-10-09 13:48:22 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kljohnsonn/bsclisp
+
+go 1.23.0
---
+++ input.lisp
@@ -0,0 +1,8 @@
+(define (factorial n)
+  (if (<= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(display (factorial 5))
+(newline)
+
